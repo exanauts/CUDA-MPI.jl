b60ebe716965ba754d989b1d1fd3ebacee4f3a71
Michel Schanen <mschanen@anl.gov>
2022-06-03 16:04:37 +0000

    Working config for ThetaGPU

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ if [ "$HOSTNAME" = moonshot ]; then
   export JULIA_CUDA_MEMORY_POOL=none
   export JULIA_MPI_BINARY=system
   export JULIA_CUDA_USE_BINARYBUILDER=false
+  julia --project -e 'using Pkg; Pkg.update()'
   julia --project -e 'using Pkg; Pkg.build("MPI"; verbose=true)'
   julia --project -e 'using Pkg; Pkg.build("CUDA"; verbose=true)'
 fi
@@ -14,6 +15,7 @@ if [ "$DOMAIN" = mcp.alcf.anl.gov ]; then
   export JULIA_CUDA_MEMORY_POOL=none
   export JULIA_MPI_BINARY=system
   export JULIA_CUDA_USE_BINARYBUILDER=false
+  julia --project -e 'using Pkg; Pkg.update()'
   julia --project -e 'using Pkg; Pkg.build("MPI"; verbose=true)'
   julia --project -e 'using Pkg; Pkg.build("CUDA"; verbose=true)'
 fi
